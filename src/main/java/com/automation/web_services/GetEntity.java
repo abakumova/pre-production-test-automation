40f6480ec9e0ae91573273ebacde31d52264d6a0
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-01-11 20:29:41 +0200

    WebService files were added

---
+++ src/main/java/com/automation/web_services/GetEntity.java
@@ -0,0 +1,12 @@
+package com.automation.web_services;
+
+import lombok.Data;
+
+@Data
+public class GetEntity {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+}
